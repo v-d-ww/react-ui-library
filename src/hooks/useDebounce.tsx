8e50bf9ace6db0ea34b6d803d4d6c3d97c5d64ba
v-d-ww <2044309490@qq.com>
2025-12-03 16:20:16 +0800

    chore: prepare for release

--- src/hooks/useDebounce.tsx
+++ src/hooks/useDebounce.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react"
 export function useDebounce(value: any, delay = 300) {
   const [debounceValue, setDebounceValue] = useState(value)
   useEffect(() => {
-    const handler = window.setTimeout(() => {
+    const handler = setTimeout(() => {
       setDebounceValue(value)
     }, delay)
     return () => clearTimeout(handler)
